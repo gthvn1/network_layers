2c5c7e99eaf3037f581db2876b127127f1aaf454
Guillaume <guillaume.thouvenin@vates.tech>
2025-11-05 09:20:02 +0100

    Change IP address in params.zig

--- src/params.zig
+++ src/params.zig
@@ -1,3 +1,3 @@
 pub const iface = "veth0-peer";
 pub const mac: []const u8 = &[_]u8{ 0x96, 0x6a, 0x35, 0x42, 0xad, 0x06 };
-pub const ip = [_]u8{ 192, 168, 0, 3 };
+pub const ip = [_]u8{ 192, 168, 38, 3 };
